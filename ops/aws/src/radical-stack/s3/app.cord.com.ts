fd51563c935e67855925f6489419a7c05b95f8f3
Adam Vartanian <flooey@cord.com>
2024-07-19 16:15:48 +0100

    Don't customize app.cord.com bucket name

--- ops/aws/src/radical-stack/s3/app.cord.com.ts
+++ ops/aws/src/radical-stack/s3/app.cord.com.ts
@@ -5,11 +5,11 @@ import { radicalStack } from 'ops/aws/src/radical-stack/stack.ts';
 import { CORS_RULES } from 'ops/aws/src/radical-stack/s3/S3Config.ts';
 import { vanta } from 'ops/aws/src/radical-stack/vanta.ts';
 import { originAccessIdentity } from 'ops/aws/src/radical-stack/cloudfront/common.ts';
-import { PRIMARY_DOMAIN_NAME, S3_BUCKET_PREFIX } from 'ops/aws/src/radical-stack/Config.ts';
+import { PRIMARY_DOMAIN_NAME } from 'ops/aws/src/radical-stack/Config.ts';
 
 function makeBucket(id: string, bucketName: string) {
   const bucket = new S3.Bucket(radicalStack(), id, {
-    bucketName: `${S3_BUCKET_PREFIX}${bucketName}`,
+    bucketName,
     blockPublicAccess: S3.BlockPublicAccess.BLOCK_ALL,
     cors: CORS_RULES,
     encryption: S3.BucketEncryption.S3_MANAGED,
