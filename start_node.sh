b3ed1110d77bd92a723ae34d666c0d5303a3e1d6
Zef Lin <zef@fabi.ai>
2024-07-23 13:59:23 -0700

    start app and async worker on start_node.sh (#6)

--- start_node.sh
+++ start_node.sh
@@ -5,7 +5,9 @@ cp localhost/localhost.crt dist/server/
 
 while true; do
   echo "Starting the process..."
-  npm run start-server-prod
+  npm run start-external-dev &
+  npm run start-server-prod &
+  npm run start-asyncWorker-prod
   echo "Process has failed. Restarting in 5 seconds..."
   sleep 5
 done
